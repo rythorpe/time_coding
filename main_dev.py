4f875c668491d7d3f64696abde18213917ed2818
Ryan Thorpe <ryvthorpe@gmail.com>
2024-09-17 16:22:43 -0600

    add pytorch api tutorial, main dev script, and utils module

---
+++ main_dev.py
@@ -0,0 +1,16 @@
+"""Main development script for project."""
+
+# %%
+import numpy as np
+import matplotlib.pyplot as plt
+
+import torch
+from torch import nn
+
+from utils import get_device
+
+
+# %%
+device = get_device()
+
+# %%
---
+++ utils.py
@@ -0,0 +1,16 @@
+"""Utility variables and functions."""
+
+import torch
+
+
+def get_device():
+    # Get cpu, gpu or mps device for training.
+    device = (
+        "cuda"
+        if torch.cuda.is_available()
+        else "mps"
+        if torch.backends.mps.is_available()
+        else "cpu"
+    )
+    print(f"Using {device} device")
+    return device
