62b3a4a4c60ef47667c00a9190c066bb6137e021
Ryan Thorpe <ryvthorpe@gmail.com>
2024-09-25 16:51:53 -0600

    WIP: work on RNN_echostate

--- models.py
+++ models.py
@@ -34,35 +34,42 @@ class RNN_echostate(nn.Module):
     def __init__(self, n_inputs=1, n_rec_units=100, n_outputs=1):
         super().__init__()
         self.n_rec_units = n_rec_units
+        self.n_outputs = n_outputs
         self.noise_std = 0.001
+        self.tau = 0.01  # 10 ms
         # self.h_0 = torch.zeros(n_rec_units)
-        self.W_ih = nn.Parameter()
-        self.W_hh = nn.Parameter()
+        W_ih = torch.empty(n_inputs,n_rec_units)
+        torch.nn.init.normal_(W_ih)
+        self.W_ih = nn.Parameter(W_ih, requires_grad=False)
+        self.W_hh = nn.Parameter(, requires_grad=False)
+        self.W_hz = nn.Parameter(, requires_grad=True)
 
-    def forward(self, x):
-        noise = torch.randn_like(x) * self.noise_std
-        h_t, _ = self.rec_layer(x + noise)
-        return self.output_layer(h_t), h_t
-
-
-    def forward(self, x, h_0=None):
+    def forward(self, x, h_0=None, dt=0.001):
         # transpose so it is time x batches x n_inputs
         x = x.transpose(0, 1)
         seq_len, batch_size, _ = x.size()
         if h_0 is None:
             h_0 = torch.zeros(batch_size, self.n_rec_units)
         h_t_minus_1 = h_0
-        h_t = h_0
-        output = []
+        h_transfer = torch.tanh(h_0)
+        h = torch.zeros(batch_size, seq_len, self.n_rec_units)
+        z = torch.zeros(batch_size, seq_len, self.n_outputs)
         for t in range(seq_len):
             noise = torch.randn(self.n_rec_units) * self.noise_std
-            h_t = (x[t] @ self.W_ih.T
-                + torch.tanh(h_t_minus_1) @ self.W_hh.T
-                + noise
-            )
-            output.append(h_t[-1])
+            # compute h output here so that it can be passed to both
+            # z (output unit) and itself on the next time step
+            h_transfer = torch.tanh(h_t_minus_1)
+
+            z_t = h_t_minus_1 @ self.W_hz.T
+            dhdt = (-h_t_minus_1 + h_transfer @ self.W_hh.T
+                    + x[t] @ self.W_ih.T
+                    + z_t_minus_1 @ self.W_zh.T
+                    + noise) / self.tau
+            h_t = h_t_minus_1 + dhdt * dt
+
+            h[:, t, :] = h_t
             h_t_minus_1 = h_t
-        output = torch.stack(output)
-        # transpose back so it is batches x time x n_inputs
-        output = output.transpose(0, 1)
-        return output, h_t
+
+            z[:, t, :] = z_t
+            z_t_minus_1 = z_t
+        return z, h
