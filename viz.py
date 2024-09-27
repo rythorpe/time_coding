11c19b79e04325336e873db47d7bb705cdba0708
Ryan Thorpe <ryvthorpe@gmail.com>
2024-09-26 20:32:31 -0600

    fix limits, yticks, and recurrent trajectory plotting

--- viz.py
+++ viz.py
@@ -5,18 +5,22 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 
-def plot_inputs_outputs(inputs, outputs, times,
-                        rec_traj=None):
+def plot_inputs_outputs(inputs, outputs, times, rec_traj=None):
     n_trials, n_times, n_inputs = inputs.shape
     _, _, n_outputs = outputs.shape
 
     n_plots = 2
     if rec_traj is not None:
         n_plots += n_trials
+        n_hidden = rec_traj.shape[2]
+        n_hidden_plot = 10  # number of hidden units to plot
+        if n_hidden < 10:
+            n_hidden_plot = n_hidden
 
     colors = plt.cm.binary(np.linspace(0.2, 1, n_trials))
     # colors = plt.cm.viridis(np.linspace(0, 1, n_trials))
-    fig, axes = plt.subplots(n_plots, 1, sharex=True, figsize=(6, 6))
+    fig, axes = plt.subplots(n_plots, 1, sharex=True, sharey=True,
+                             figsize=(6, 6))
 
     for trial_idx, color in zip(range(n_trials), colors):
         # inputs
@@ -24,20 +28,21 @@ def plot_inputs_outputs(inputs, outputs, times,
 
         # recurrent unit trajectories for each trial
         if rec_traj is not None:
-            axes[trial_idx + 1].plot(times, rec_traj[trial_idx, :, :],
+            axes[trial_idx + 1].plot(times,
+                                     rec_traj[trial_idx, :, :n_hidden_plot],
                                      c=color)
-            axes[trial_idx + 1].set_yticks([-1, 0, 1])
+            # axes[trial_idx + 1].set_yticks([-1, 0, 1])
             axes[trial_idx + 1].set_ylabel('h(t)')
 
         # outputs
         axes[-1].plot(times, outputs[trial_idx, :, :], c=color, lw=2)
 
-    axes[0].set_yticks([-2, 0, 2])
+    # axes[0].set_yticks([-2, 0, 2])
     axes[0].set_ylabel('input')
 
-    axes[-1].set_xticks(np.arange(0, np.round(times[-1] + 0.2), 0.2))
+    axes[-1].set_xticks(np.arange(0, times[-1] + 0.2, 0.2))
     axes[-1].set_xlabel('time (s)')
-    axes[-1].set_yticks([0, 1])
+    # axes[-1].set_yticks([0, 1])
     axes[-1].set_ylabel('output')
 
     return fig
