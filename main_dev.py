ad0677a67b6d09a4afaea924d7d1c333e8ad6781
Ryan Thorpe <ryvthorpe@gmail.com>
2024-09-30 13:29:35 -0600

    refine train and test functions

--- main_dev.py
+++ main_dev.py
@@ -8,19 +8,21 @@ import torch
 from torch import nn
 
 from utils import get_device
-from models import RNN_echostate
+from models import RNN
 from viz import plot_inputs_outputs
 
 
 # %% set meta-parameters
 device = get_device()
+device = 'cpu'
 torch.random.manual_seed(1234)  # for reproducibility while troubleshooting
 
 
 # %% instantiate model, loss function, and optimizer
 n_inputs, n_hidden, n_outputs = 1, 1000, 1
-model = RNN_echostate(n_inputs=1, n_hidden=n_hidden, n_outputs=1,
-                      silence_feedback=False).to(device)
+model = RNN(n_inputs=n_inputs, n_hidden=n_hidden,
+            n_outputs=n_outputs, echo_state=True)
+model.to(device)
 print(model)
 
 loss_fn = nn.MSELoss()
@@ -29,7 +31,7 @@ optimizer = torch.optim.SGD(model.parameters(), lr=1e-3)
 
 # %% create data
 # proxy for precision of timing code (increase for more precision)
-n_amplitudes = 1
+n_amplitudes = 12
 min_perturb, max_perturb = -2.0, 2.0
 
 dt = 1e-3  # 1 ms
@@ -47,8 +49,8 @@ data_x[:, perturb_win_mask, :] = torch.tile(
     (1, np.sum(perturb_win_mask), n_inputs)
 )
 
-# delays = np.linspace(0.1, tstop - 0.1, n_amplitudes)  # add margins
-delays = [0.5]
+delays = np.linspace(0.1, tstop - 0.1, n_amplitudes)  # add margins
+# delays = [0.5]
 data_y = torch.zeros((n_amplitudes, n_times, n_outputs))
 k_w = 50  # numel of a given side of the kernel
 gaussian_kernel = np.exp(np.arange(-k_w * dt, k_w * dt, dt) ** 2 /
@@ -74,13 +76,13 @@ fig.show()
 
 # %% define train and test functions that will loop over
 # each batch
-def train(X, Y, model, loss_fn, optimizer, h_0=None):
+def train(inputs, targets, times, model, loss_fn, optimizer, h_0=None):
     model.train()
-    X, Y = X.to(device), Y.to(device)
+    inputs, targets = inputs.to(device), targets.to(device)
 
     # run model without storing gradients until t=0
     with torch.no_grad():
-        pred, h_t = model(X[:, times <= 0, :], h_0=h_0, dt=dt)
+        outputs, h_t = model(inputs[:, times <= 0, :], h_0=h_0, dt=dt)
 
     # now, train using FORCE
     force_step = 2
@@ -88,12 +90,12 @@ def train(X, Y, model, loss_fn, optimizer, h_0=None):
     t_0_idx = np.nonzero(times > 0)[0][0]
     for t_idx in np.arange(t_0_idx + force_step, n_times + force_step,
                            force_step):
-        # Compute prediction error
+        # compute prediction error
         t_minus_1_idx = t_idx - force_step
         h_0 = h_t[:, -1, :].detach()
-        pred, h_t = model(X[:, t_minus_1_idx:t_idx, :], h_0=h_0, dt=dt)
+        outputs, h_t = model(inputs[:, t_minus_1_idx:t_idx, :], h_0=h_0, dt=dt)
         # loss at t - delta_t
-        loss = loss_fn(pred[:, 0, :], Y[:, t_minus_1_idx, :])
+        loss = loss_fn(outputs[:, 0, :], targets[:, t_minus_1_idx, :])
         # backpropagation
         loss.backward()
         optimizer.step()
@@ -103,21 +105,21 @@ def train(X, Y, model, loss_fn, optimizer, h_0=None):
     return losses
 
 
-def test(X, Y, model, loss_fn, h_0=None):
+def test(inputs, targets, times, model, loss_fn, h_0=None):
     model.eval()
 
     with torch.no_grad():
-        X, Y = X.to(device), Y.to(device)
+        inputs, targets = inputs.to(device), targets.to(device)
 
         # Compute prediction error
-        pred, h_t = model(X, h_0=h_0, dt=dt)
-        loss = loss_fn(pred, Y)
+        outputs, h_t = model(inputs, h_0=h_0, dt=dt)
+        loss = loss_fn(outputs, targets)
 
-        input = X.cpu()
-        pred = pred.cpu()
+        inputs = inputs.cpu()
+        outputs = outputs.cpu()
         h_t = h_t.cpu()
 
-        fig = plot_inputs_outputs(input, pred, times, rec_traj=h_t)
+        fig = plot_inputs_outputs(inputs, outputs, times, rec_traj=h_t)
         fig.show()
 
     print(f"Test loss: {loss.item():>7f}")
@@ -126,14 +128,14 @@ def test(X, Y, model, loss_fn, h_0=None):
 # fix initial conditions for training and testing
 h_0 = (torch.rand(n_hidden) * 2) - 1  # uniform in (-1, 1)
 h_0 = torch.tile(h_0, (n_amplitudes, 1))  # replicate for each batch
-h_0.to(device)
+h_0 = h_0.to(device)
 
 # %% train and test model over a few epochs
-n_iter = 5
+n_iter = 10
 loss_per_iter = list()
 for t in range(n_iter):
     print(f"Iteration {t+1}\n-------------------------------")
-    loss = train(data_x, data_y, model, loss_fn, optimizer, h_0=h_0)
+    loss = train(data_x, data_y, times, model, loss_fn, optimizer, h_0=h_0)
     loss_per_iter.extend(loss)
     # test(test_dataloader, model, loss_fn)
 print("Done!")
@@ -142,6 +144,6 @@ plt.figure()
 plt.plot(loss_per_iter)
 
 # %%
-test(data_x, data_y, model, loss_fn, h_0=h_0)
+test(data_x, data_y, times, model, loss_fn, h_0=h_0)
 
 # %%
--- models.py
+++ models.py
@@ -7,39 +7,15 @@ from torch import nn
 
 
 class RNN(nn.Module):
-    def __init__(self, n_inputs, n_outputs):
-        super().__init__()
-        n_rec_units = 100
-        self.noise_std = 0.001
-        # self.h_0 = torch.zeros(n_rec_units)
-        self.rec_layer = nn.RNN(input_size=n_inputs,
-                                hidden_size=n_rec_units,
-                                nonlinearity='tanh',
-                                bias=False,
-                                batch_first=True)
-        for param in self.rec_layer.parameters():
-            param.detach_()
-        self.output_layer = nn.Sequential(
-            nn.Linear(in_features=n_rec_units,
-                      out_features=n_outputs,
-                      bias=False),
-            nn.Tanh()
-        )
-
-    def forward(self, x):
-        noise = torch.randn_like(x) * self.noise_std
-        h_t, _ = self.rec_layer(x + noise)
-        return self.output_layer(h_t), h_t
-
-
-class RNN_echostate(nn.Module):
     def __init__(self, n_inputs=1, n_hidden=300, n_outputs=1,
-                 silence_feedback=False):
+                 echo_state=True):
         super().__init__()
         self.n_hidden = n_hidden
         self.n_outputs = n_outputs
         self.noise_std = 0.0  # 0.001
         self.tau = 0.01  # 10 ms
+        gain = 1.5
+        prob_c = 0.10
 
         self.W_ih = nn.Parameter(torch.empty(n_hidden, n_inputs),
                                  requires_grad=False)
@@ -55,8 +31,6 @@ class RNN_echostate(nn.Module):
         torch.nn.init.normal_(self.W_ih, mean=0.0, std=w_input_std)
 
         # initialize hidden weights
-        gain = 1.5
-        prob_c = 0.10
         w_hidden_std = gain / np.sqrt(prob_c * n_hidden)
         torch.nn.init.sparse_(self.W_hh, sparsity=(1 - prob_c),
                               std=w_hidden_std)
@@ -74,10 +48,15 @@ class RNN_echostate(nn.Module):
         torch.nn.init.normal_(self.W_hz, mean=0.0, std=w_output_std)
 
         # initialize feedback (echo) weights
-        if silence_feedback:
-            torch.nn.init.zeros_(self.W_zh)
-        else:
+        if echo_state:
             torch.nn.init.uniform_(self.W_zh, a=-1.0, b=1.0)
+        else:
+            torch.nn.init.zeros_(self.W_zh)
+
+        # create registered buffers (fancy attributes) that need to live on the
+        # same device as self
+        # self.register_buffer('h_0', torch.zeros(self.n_hidden))
+        self.register_buffer('noise', torch.zeros(self.n_hidden))
 
     def forward(self, x, h_0=None, dt=0.001):
         # assuming batches x time x n_inputs
@@ -86,9 +65,12 @@ class RNN_echostate(nn.Module):
         if h_0 is None:
             h_0 = (torch.rand(self.n_hidden) * 2) - 1  # uniform in (-1, 1)
             h_0 = torch.tile(h_0, (batch_size, 1))  # replicate for each batch
+
         h = torch.zeros(batch_size, seq_len, self.n_hidden)
         z = torch.zeros(batch_size, seq_len, self.n_outputs)
 
+        # NB: doesn't work on CUDA; due to FORCE training, h_0 is updated
+        # regularly in time and therefore lives on the CPU 
         for batch_idx in range(batch_size):
             # initialize hidden and output states
             # each batch can theoretically have a different start point
@@ -97,11 +79,11 @@ class RNN_echostate(nn.Module):
             z_t_minus_1 = h_transfer @ self.W_hz.T
             # begin integration over time
             for t in range(seq_len):
-                noise = torch.randn(self.n_hidden) * self.noise_std
+                self.noise.normal_(0, self.noise_std)
                 dhdt = (-h_t_minus_1 + h_transfer @ self.W_hh.T
                         + x[batch_idx, t] @ self.W_ih.T
                         + z_t_minus_1 @ self.W_zh.T
-                        + noise) / self.tau
+                        + self.noise) / self.tau
                 h_t = h_t_minus_1 + dhdt * dt
                 h[batch_idx, t, :] = h_t
                 h_t_minus_1 = h_t
