ac47d6f9189951c6175370a2c41722e45a7af44c
Ryan Thorpe <ryvthorpe@gmail.com>
2024-10-14 16:55:01 -0600

    use set_optimimal_w_out() to determine optimal output weights

--- main_dev.py
+++ main_dev.py
@@ -53,7 +53,7 @@ data_x[:, perturb_win_mask, :] = torch.tile(
 
 # delays = np.linspace(0.1, tstop - 0.1, n_amplitudes)  # add margins
 delays = [0.5]
-width = 0.100  # 20 ms
+width = 0.02  # 20 ms
 data_y = torch.zeros((n_amplitudes, n_times, n_inputs))
 for output_idx, center in enumerate(delays):
     data_y[output_idx, :, 0] = torch.tensor(gaussian_func(times, center, width))
@@ -101,7 +101,6 @@ def train_intime(inputs, targets, times, model, loss_fn, optimizer, h_0=None):
         outputs, h_t = model(inputs[:, times <= 0, :], h_0=h_0, dt=dt)
 
     # now, train using FORCE
-    force_step = 1
     losses = list()
     t_0_idx = np.nonzero(times > 0)[0][0]
     for t_idx in np.arange(t_0_idx, n_times):
@@ -142,17 +141,53 @@ def test(inputs, targets, times, model, loss_fn, h_0=None):
 
         # Compute prediction error
         outputs, h_t = model(inputs, h_0=h_0, dt=dt)
-        loss = loss_fn(outputs, targets)
+        loss = loss_fn(outputs[:, times > 0, :], targets[:, times > 0, :])
 
         inputs = inputs.cpu()
         outputs = outputs.cpu()
         h_t = h_t.cpu()
 
-        fig = plot_inputs_outputs(inputs, outputs, times, rec_traj=h_t,
-                                  targets=targets)
-        fig.show()
-
+    fig = plot_inputs_outputs(inputs, outputs, times, rec_traj=h_t,
+                              targets=targets)
+    fig.show()
     print(f"Test loss: {loss.item():>7f}")
+
+
+def set_optimimal_w_out(inputs, targets, times, model, loss_fn, h_0):
+    model.eval()
+
+    with torch.no_grad():
+        inputs, targets = inputs.to(device), targets.to(device)
+
+        # Compute prediction error
+        outputs, h_t = model(inputs, h_0=h_0, dt=dt)
+        loss = loss_fn(outputs[:, times > 0, :], targets[:, times > 0, :])
+        print(f"Initial loss: {loss.item():>7f}")
+        activation = torch.nn.Tanh()
+        h_transfer = activation(h_t)
+
+        inputs = inputs.cpu()
+        outputs = outputs.cpu()
+        h_t = h_t.cpu()
+        # assuming input/output size of 1, select first and only batch
+        h_transfer = h_transfer.cpu()[0, times > 0, :]
+
+        cov = h_transfer.T @ h_transfer
+        targets_ = targets[0, times > 0, 0:1]  # column vector
+        # W_hz_ = cov.inverse() @ (h_transfer.T @ targets_)
+        W_hz_ = torch.linalg.solve(cov, (h_transfer.T @ targets_))
+        model.W_hz[:] = W_hz_.T
+        h_argmax = torch.argmax(model.W_hz.abs())
+
+        outputs, h_t = model(inputs, h_0=h_0, dt=dt)
+        plt.figure()
+        plt.plot(times[times > 0], h_transfer[:, h_argmax])
+        loss = loss_fn(outputs[:, times > 0, :], targets[:, times > 0, :])
+        print(f"Final loss: {loss.item():>7f}")
+
+    fig = plot_inputs_outputs(inputs, outputs, times, rec_traj=h_t,
+                              targets=targets)
+    fig.show()
 
 
 # fix initial conditions for training and testing
@@ -161,23 +196,24 @@ h_0 = torch.tile(h_0, (n_amplitudes, 1))  # replicate for each batch
 h_0 = h_0.to(device)
 
 # %% train and test model over a few epochs
-n_iter = 200
-loss_per_iter = list()
-for t in range(n_iter):
-    print(f"Iteration {t+1}\n-------------------------------")
-    # loss = train(data_x, data_y, times, model, loss_fn, optimizer, h_0=h_0)
-    # loss = train_force(data_x, data_y, times, model, loss_fn, optimizer, h_0=h_0)
-    total_loss = train_intime(data_x, data_y, times, model, loss_fn, optimizer, h_0=h_0)
-    loss_per_iter.append(total_loss)
-    # test(test_dataloader, model, loss_fn)
-print("Done!")
+# n_iter = 200
+# loss_per_iter = list()
+# for t in range(n_iter):
+#     print(f"Iteration {t+1}\n-------------------------------")
+#     # loss = train(data_x, data_y, times, model, loss_fn, optimizer, h_0=h_0)
+#     # loss = train_force(data_x, data_y, times, model, loss_fn, optimizer, h_0=h_0)
+#     total_loss = train_intime(data_x, data_y, times, model, loss_fn, optimizer, h_0=h_0)
+#     loss_per_iter.append(total_loss)
+#     # test(test_dataloader, model, loss_fn)
+# print("Done!")
 
-plt.figure()
-plt.plot(loss_per_iter)
-plt.xlabel('iteration')
-plt.ylabel('loss')
+# plt.figure()
+# plt.plot(loss_per_iter)
+# plt.xlabel('iteration')
+# plt.ylabel('loss')
 
 # %%
 test(data_x, data_y, times, model, loss_fn, h_0=h_0)
+set_optimimal_w_out(data_x, data_y, times, model, loss_fn, h_0=h_0)
 
 # %%
