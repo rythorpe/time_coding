f4ee385a22cae9be2b71b29990e76d65eb222b3c
Ryan Thorpe <ryvthorpe@gmail.com>
2024-10-28 00:11:19 -0600

    WIP: run many trials of random nets for each param sample

--- main_dev.py
+++ main_dev.py
@@ -20,7 +20,8 @@ torch.random.manual_seed(95214)
 np.random.seed(35107)
 
 # define parameter sweep
-n_samp = 2
+n_samp = 5
+n_nets_per_samp = 10
 params = {'n_outputs': np.linspace(5, 25, n_samp),
           'targ_std': np.linspace(0.005, 0.025, n_samp)}
 xx, yy = np.meshgrid(params['n_outputs'], params['targ_std'])
@@ -29,69 +30,84 @@ param_vals = [pt for pt in zip(xx.flatten(), yy.flatten())]
 for sample_idx, param_sample in enumerate(param_vals):
     n_outputs, targ_std = int(param_sample[0]), param_sample[1]
 
-    # instantiate model, loss function, and optimizer
-    # n_inputs, n_hidden, n_outputs = 1, 300, 10
-    n_inputs, n_hidden = 1, 300
-    model = RNN(n_inputs=n_inputs, n_hidden=n_hidden,
-                n_outputs=n_outputs, echo_state=False)
-    model.to(device)
-    print(model)
+    # observe a few random nets for each parameter sample
+    for trial_idx in range(n_nets_per_samp):
 
-    loss_fn = nn.MSELoss()
-    optimizer = torch.optim.SGD(model.parameters(), lr=1e-3)
+        # instantiate model, loss function, and optimizer
+        # n_inputs, n_hidden, n_outputs = 1, 300, 10
+        n_inputs, n_hidden = 1, 300
+        model = RNN(n_inputs=n_inputs, n_hidden=n_hidden,
+                    n_outputs=n_outputs, echo_state=False)
+        model.to(device)
+        # print(model)
 
-    # set parameters
-    # simulation parameters
-    dt = 1e-3  # 1 ms
-    tstop = 1.1  # 1 sec
-    times = np.arange(-0.1, tstop, dt)
-    n_times = len(times)
+        loss_fn = nn.MSELoss()
+        optimizer = torch.optim.SGD(model.parameters(), lr=1e-3)
 
-    # define inputs (for contextual modulation / recurrent perturbations)
-    n_batches = 1
-    inputs = torch.zeros((n_batches, n_times, n_inputs))
+        # set parameters
+        # simulation parameters
+        dt = 1e-3  # 1 ms
+        tstop = 1.1  # 1 sec
+        times = np.arange(-0.1, tstop, dt)
+        n_times = len(times)
 
-    # define output targets
-    output_delays = np.linspace(0.1, tstop - 0.1, n_outputs)  # w/ margins
-    targets = torch.zeros((n_batches, n_times, n_outputs))
-    for output_idx, center in enumerate(output_delays):
-        targets[0, :, output_idx] = torch.tensor(gaussian(times, center, targ_std))
+        # define inputs (for contextual modulation / recurrent perturbations)
+        n_batches = 1
+        inputs = torch.zeros((n_batches, n_times, n_inputs))
+
+        # define output targets
+        output_delays = np.linspace(0.1, tstop - 0.1, n_outputs)  # w/ margins
+        targets = torch.zeros((n_batches, n_times, n_outputs))
+        for output_idx, center in enumerate(output_delays):
+            targets[0, :, output_idx] = torch.tensor(gaussian(times, center, targ_std))
 
 
-    # set initial conditions of recurrent units fixed across iterations of
-    # training and testing
-    h_0 = (torch.rand(n_hidden) * 2) - 1  # uniform in (-1, 1)
-    h_0 = torch.tile(h_0, (n_batches, 1))  # replicate for each batch
+        # set initial conditions of recurrent units fixed across iterations of
+        # training and testing
+        h_0 = (torch.rand(n_hidden) * 2) - 1  # uniform in (-1, 1)
+        h_0 = torch.tile(h_0, (n_batches, 1))  # replicate for each batch
 
-    # run opt routine
-    # move to desired device
-    inputs, targets, h_0 = inputs.to(device), targets.to(device), h_0.to(device)
+        # run opt routine
+        # move to desired device
+        inputs, targets, h_0 = inputs.to(device), targets.to(device), h_0.to(device)
 
-    # plot model output before training
-    test(inputs, targets, times, model, loss_fn, h_0=h_0)
+        # plot model output before training
+        test(inputs, targets, times, model, loss_fn, h_0=h_0)
 
-    # train model weights
-    max_iter = 300
-    convergence_reached = False
-    loss_per_iter = list()
-    for t in range(max_iter):
-        print(f"Iteration {t + 1}")
-        loss, param_dist = train(inputs, targets, times, model, loss_fn, optimizer, h_0=h_0)
-        loss_per_iter.append(loss)
-        if param_dist < 3e-4:
-            convergence_reached = True
-            break
-    print(f"Trial {sample_idx} training complete!!")
-    if not convergence_reached:
-        print(f"Warning: didn't converge (param_dist={param_dist})!!")
+        # train model weights
+        max_iter = 300
+        convergence_reached = False
+        loss_per_iter = list()
+        for t in range(max_iter):
+            print(f"Iteration {t + 1}")
+            loss, param_dist = train(inputs, targets, times, model, loss_fn, optimizer, h_0=h_0)
+            loss_per_iter.append(loss)
+            if param_dist < 3e-4:
+                convergence_reached = True
+                break
+        print(f"Trial {sample_idx} training complete!!")
+        if not convergence_reached:
+            print(f"Warning: didn't converge (param_dist={param_dist})!!")
 
-    plt.figure()
-    plt.plot(loss_per_iter)
-    plt.xlabel('iteration')
-    plt.ylabel('loss')
+        plt.figure()
+        plt.plot(loss_per_iter)
+        plt.xlabel('iteration')
+        plt.ylabel('loss')
 
-    # investigate fitted model
-    # plot model output after training
-    test(inputs, targets, times, model, loss_fn, h_0=h_0)
-    # solve for optimal model output weights given hidden unit responses
-    set_optimimal_w_out(inputs, targets, times, model, loss_fn, h_0=h_0)
+        # investigate fitted model
+        # plot model output after training
+        test(inputs, targets, times, model, loss_fn, h_0=h_0)
+
+        # calculate metrics-of-interest for fitted model sim
+        # recurrent correlation matrix
+        # recurrent spectrum
+        # mutual entropy (recurrent and target gaussian)
+        # n_convergence_iters
+        # final MSE
+        # final avg std
+        # final std as a function of latency
+
+        # solve for optimal model output weights given hidden unit responses
+        set_optimimal_w_out(inputs, targets, times, model, loss_fn, h_0=h_0)
+
+        # baseline noise
