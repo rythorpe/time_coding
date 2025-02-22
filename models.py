067ea01ea2f0eeecc4842b8717c8a6c016d05ecc
Ryan Thorpe <ryvthorpe@gmail.com>
2025-02-21 22:18:31 -0700

    add STP to RNN model

--- models.py
+++ models.py
@@ -14,9 +14,13 @@ class RNN(nn.Module):
         self.n_outputs = n_outputs
         self.noise_std = 0.0  # 0.001
         self.tau = 0.01  # 10 ms
-        gain = 1.6
+        self.tau_depr = 0.2  # 200 ms; taken from Mongillo et al. Science 2008
+        self.tau_facil = 1.5  # 1.5 s
+        gain = 1.8
         prob_c = 0.3
 
+        self.p_rel = nn.Parameter(torch.empty(n_hidden), requires_grad=False)
+        self.tau_depr = nn.Parameter(torch.empty(n_hidden), requires_grad=False)
         self.W_ih = nn.Parameter(torch.empty(n_hidden, n_inputs),
                                  requires_grad=False)
         self.W_hh = nn.Parameter(torch.empty(n_hidden, n_hidden),
@@ -25,6 +29,11 @@ class RNN(nn.Module):
                                  requires_grad=True)
         self.W_zh = nn.Parameter(torch.empty(n_hidden, n_outputs),
                                  requires_grad=False)
+        
+        # initialize release probabilities
+        # Bounds taken from Tsodyks & Markram PNAS 1997
+        torch.nn.init.uniform_(self.p_rel, a=0.1, b=0.95)
+        torch.nn.init.uniform_(self.tau_depr, a=0.01, b=0.2)
 
         # initialize input weights
         w_input_std = 1 / np.sqrt(n_hidden)
@@ -64,6 +73,7 @@ class RNN(nn.Module):
         # assuming batches x time x n_inputs
         batch_size, seq_len, _ = x.size()
 
+        u = torch.zeros(batch_size, seq_len, self.n_hidden)
         h = torch.zeros(batch_size, seq_len, self.n_hidden)
         z = torch.zeros(batch_size, seq_len, self.n_outputs)
 
@@ -72,21 +82,31 @@ class RNN(nn.Module):
         for batch_idx in range(batch_size):
             # initialize hidden and output states
             # each batch can theoretically have a different start point
+            u_t_minus_1 = torch.tensor(self.p_rel)
             h_t_minus_1 = h_0[batch_idx, :]
             h_transfer = torch.tanh(h_0[batch_idx, :])
             z_t_minus_1 = h_transfer @ self.W_hz.T
             # begin integration over time
             for t in range(seq_len):
                 self.noise.normal_(0, self.noise_std)
+                # pre-synaptic plasticity
+                dudt = ((self.p_rel - u_t_minus_1) / self.tau_depr
+                        - self.p_rel * u_t_minus_1 * h_transfer)
+                # dudt = ((self.p_rel - u_t_minus_1) / self.tau_facil
+                #         + self.p_rel * (1 - u_t_minus_1) * h_transfer)
+                u_t = u_t_minus_1 + dudt * dt
+                u[batch_idx, t, :] = u_t
+                # post-synaptic integration
                 dhdt = (-h_t_minus_1
                         # mask to enforces static, sparse recurrent connections
-                        + h_transfer @ (self.W_hh_mask * self.W_hh).T
+                        + u_t_minus_1 * h_transfer @ (self.W_hh_mask * self.W_hh).T
                         + x[batch_idx, t] @ self.W_ih.T
                         + z_t_minus_1 @ self.W_zh.T
                         + self.noise) / self.tau
                 h_t = h_t_minus_1 + dhdt * dt
                 h[batch_idx, t, :] = h_t
                 h_t_minus_1 = h_t
+                u_t_minus_1 = u_t
                 # compute h output here so that it can be passed to both
                 # z (output unit) and itself on the next time step
                 h_transfer = torch.tanh(h_t)
