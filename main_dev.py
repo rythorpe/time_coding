e21f23227d95e5a36824e160c882adc13f64cebd
Ryan Thorpe <ryvthorpe@gmail.com>
2025-04-15 16:37:35 -0600

    migrate sim pipeline from aux_dev.ipynb to main_dev.py

--- main_dev.py
+++ main_dev.py
@@ -12,10 +12,10 @@ from joblib import Parallel, delayed
 import torch
 from torch import nn
 
-from utils import gaussian, get_gaussian_targets, get_random_targets
+from utils import gaussian, get_gaussian_targets
 from models import RNN
-from train import test, pre_train, train, train_simple, set_optimimal_w_out
-from viz import plot_stability
+from train import test_and_get_stats, pre_train, train_force, train_bptt, train_output_only
+
 
 # set meta-parameters
 # for plotting style
@@ -26,6 +26,7 @@ device = 'cpu'
 # for reproducibility while troubleshooting; numpy is for model sparse conns
 torch.random.manual_seed(95214)
 np.random.seed(35107)
+
 
 # define parameter sweep
 # n_samp = 3
@@ -54,42 +55,56 @@ def train_test_random_net(params=None, plot_sim=False):
     model.to(device)
 
     loss_fn = nn.MSELoss()
-    optimizer = torch.optim.SGD(model.parameters(), lr=1e-3)
+    optimizer = torch.optim.SGD(model.parameters(), lr=3e-1)
+    # optimizer = torch.optim.SGD(model.parameters(), lr=1e-5)
 
     # set parameters
     # simulation parameters
     dt = 1e-3  # 1 ms
-    tstop = 1.1  # 1 sec
-    times = np.arange(-0.1, tstop, dt)
+    tstop = 1.2  # 1 sec
+    times = np.arange(-0.1 + dt, tstop + dt, dt)
     n_times = len(times)
 
     # define inputs (for contextual modulation / recurrent perturbations)
     n_batches = 1
     inputs = torch.zeros((n_batches, n_times, n_inputs))
+    perturb_dur = 0.05  # 50 ms
+    perturb_win_mask = np.logical_and(times > -perturb_dur, times < 0)
+    inputs[:, perturb_win_mask, :] = 1.0
 
     # define output targets
     # set std s.t. amplitude decays to 1/e at intersection with next target
     targ_std = 0.05 / np.sqrt(2)  # ~35 ms
-    targets, delay_times_ = get_gaussian_targets(n_batches, n_outputs, times,
-                                                 targ_std)
-    # targets, opt_basis = get_random_targets(RNN, inputs,
-    #                                         (n_inputs, n_hidden, n_outputs),
-    #                                         times, n_opt_basis=n_outputs,
-    #                                         plot=True)
+    # tile center of target delays spanning sim duration (minus margins)
+    delta_delay = (tstop - 0.1) / n_outputs
+    delay_times = np.linspace(0.1, 1.0, n_outputs)
+    targets = get_gaussian_targets(n_batches, delay_times, times, targ_std)
 
     # set initial conditions of recurrent units fixed across iterations of
     # training and testing
-    h_0 = (torch.rand(n_hidden) * 2) - 1  # uniform in (-1, 1)
+    # h_0 = torch.rand(n_hidden) * 2 - 1  # uniform in (-1, 1)
+    h_0 = torch.zeros(n_hidden)
     h_0 = torch.tile(h_0, (n_batches, 1))  # replicate for each batch
+    # r_0 = torch.rand(n_hidden) * model.p_rel.detach()  # uniform in (0, p_rel)
+    r_0 = model.p_rel.detach() / (1 + model.beta * 0.5 * model.tau_depr)  # uniform in (0, p_rel)
+    r_0 = torch.tile(r_0, (n_batches, 1))
+    u_0 = torch.rand(n_hidden) # uniform in (0, 1)
+    u_0 = torch.tile(u_0, (n_batches, 1))
 
     # run opt routine
     # move to desired device
     inputs = inputs.to(device)
     targets = targets.to(device)
     h_0 = h_0.to(device)
+    r_0 = r_0.to(device)
+    # r_0 = None
+    # u_0 = u_0.to(device)
+    u_0 = None
 
     # plot model output before training
-    _, _ = test(inputs, targets, times, model, loss_fn, h_0, plot=plot_sim)
+    hidden_sr, output_sr, stats_0 = test_and_get_stats(inputs, targets, times,
+                                                       model, loss_fn, h_0,
+                                                       r_0, u_0, plot=True)
 
     # pre-train
     # max_iter_pretrain = 10
@@ -97,21 +112,33 @@ def train_test_random_net(params=None, plot_sim=False):
     #     _ = pre_train(inputs, times, model, h_0)
 
     # train model weights
-    max_iter = 40
-    convergence_reached = False
+    max_iter = 100
+    # subthresh_count = 0
+    # convergence_reached = False
     loss_per_iter = list()
     for iter_idx in range(max_iter):
         print(f"Iteration {iter_idx + 1}")
-        loss, param_dist = train(inputs, targets, times, model, loss_fn,
-                                 optimizer, h_0)
+        # loss, param_dist = train_force(inputs, targets, times, model, loss_fn,
+        #                                optimizer, h_0, r_0, u_0,
+        #                                presyn_idx=iter_idx)
+        loss, param_dist = train_bptt(inputs, targets, times, model, loss_fn,
+                                      optimizer, h_0, r_0, u_0,
+                                      p_backprop=0.2)
+        # loss, param_dist = train_output_only(inputs, targets, times, model,
+        #                                      loss_fn, optimizer, h_0, r_0, u_0)
         loss_per_iter.append(loss)
-        # if param_dist < 2e-4:
-        #     convergence_reached = True
-        #     break
+    #     if param_dist < 1e-8:
+    #         subthresh_count +=1
+    #     else:
+    #         subthresh_count = 0
+    #     if subthresh_count == 5:
+    #         convergence_reached = True
+    #         break
     # print(f"Trial {sample_idx} training complete!!")
-    if not convergence_reached:
-        print(f"Warning: didn't converge (param_dist={param_dist})!!")
+    # if not convergence_reached:
+    #     print(f"Warning: didn't converge (param_dist={param_dist})!!")
 
+    # plot loss across training
     if plot_sim:
         plt.figure()
         plt.plot(loss_per_iter)
@@ -120,38 +147,10 @@ def train_test_random_net(params=None, plot_sim=False):
 
     # investigate fitted model
     # plot model output after training
-    h_t, loss = test(inputs, targets, times, model, loss_fn, h_0,
-                     plot=plot_sim)
-    h_t_batch = h_t.cpu().squeeze()
-
-    # calculate metrics-of-interest for fitted model sim
-    ####################################################
-    # avg recurrent cross-correlation
-    xcorr = np.corrcoef(h_t_batch, rowvar=False)
-    avg_xcorr = xcorr.mean()
-    metrics['avg_xcorr'] = avg_xcorr
-
-    # spectral overlap
-    fs = 1 / dt
-    freqs_, targ_spec = periodogram(gaussian(times[times > 0],
-                                             tstop / 2, targ_std), fs=fs)
-    hidden_specs = list()
-    for h_ts in h_t_batch.T:
-        freqs_, hidden_spec = periodogram(h_ts[times > 0], fs=fs)
-        hidden_specs.append(hidden_spec)
-    hidden_spec = np.mean(hidden_specs, axis=0)
-    # normalize spectral densities
-    targ_spec /= np.sum(targ_spec)
-    hidden_spec /= np.sum(hidden_spec)
-    spec_overlap = np.sum(np.min([targ_spec, hidden_spec], axis=0))
-    metrics['spec_overlap'] = spec_overlap
-    # n_convergence_iters
-    n_iters = iter_idx
-    metrics['n_iters'] = n_iters
-    metrics['convergence'] = int(convergence_reached)
-
-    # final MSE
-    metrics['final_mse'] = float(loss)
+    hidden_sr, output_sr, stats_1 = test_and_get_stats(inputs, targets, times,
+                                                       model, loss_fn, h_0,
+                                                       r_0, u_0,
+                                                       plot=plot_sim)
 
     # temporal stability: MSE as a function of latency with t<0 perturbations
     n_tests_per_net = 10
@@ -169,8 +168,10 @@ def train_test_random_net(params=None, plot_sim=False):
             inputs[:, perturb_win_mask, :] = perturb_mag
 
             loss_fn_itemized = nn.MSELoss(reduction='none')
-            _, loss = test(inputs, targets, times, model, loss_fn_itemized,
-                           h_0, plot=False)
+            _, _, stats = test_and_get_stats(inputs, targets, times, model,
+                                             loss_fn_itemized, h_0, r_0, u_0,
+                                             plot=plot_sim)
+            loss = stats['loss']
             # weight loss by Gaussian target over time
             loss = loss * targets[:, times > 0, :]
             loss_vs_perturb[test_idx, perturb_idx, :] = loss.mean(dim=[0, 1])
@@ -178,15 +179,8 @@ def train_test_random_net(params=None, plot_sim=False):
     stability = (np.tile(loss_vs_perturb[0, :], [n_perturb, 1]) /
                  loss_vs_perturb)
     metrics['stability'] = stability
-    metrics['delay_times'] = delay_times_
+    metrics['delay_times'] = delay_times
 
-    # solve for optimal model output weights given hidden unit responses
-    outputs = set_optimimal_w_out(inputs, targets, times, model, loss_fn,
-                                  h_0=h_0, plot=plot_sim)
-    # baseline noise
-    outputs_batch = outputs.cpu().squeeze()
-    output_baseline_noise = outputs_batch[times <= 0, :].std()
-    metrics['output_baseline_noise'] = float(output_baseline_noise)
     ####################################################
     return metrics
 
