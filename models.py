229ddabcdb6c17b735ecc5c72a34ca26c6ea9d71
Ryan Thorpe <ryvthorpe@gmail.com>
2025-04-24 09:24:36 -0600

    remove renormalization of spikerates in STP equations; use full Tsodyks-Markram model with facilitation and depression

--- models.py
+++ models.py
@@ -22,15 +22,15 @@ class RNN(nn.Module):
         self.tau = 0.01  # 10 ms
         self.tau_depr = 0.2  # 200 ms; taken from Mongillo et al. Science 2008
         self.tau_facil = 1.5  # 1.5 s
-        self.beta = 10.0
+        self.beta = 20.0
         self.effective_gain = 1.6
-        stp_gain_adjustment = 1 / (0.5 / (1 + self.beta * 0.5 * self.tau_depr))
+        # stp_gain_adjustment = 1 / (0.5 / (1 + self.beta * 0.5 * self.tau_depr))
+        stp_gain_adjustment = 1 / 0.5
         self.gain = self.effective_gain * stp_gain_adjustment
         prob_c = 0.10
 
         # constant network parameters
         self.p_rel = torch.empty(n_hidden)
-        # self.tau_depr = torch.empty(n_hidden)
         self.W_ih = torch.empty(n_hidden, n_inputs)
         self.W_hh = torch.empty(n_hidden, n_hidden)
 
@@ -82,12 +82,13 @@ class RNN(nn.Module):
         # for calculation of long-term Jacobian across total sim time
         dhdhtminus1_all = torch.zeros(batch_size, seq_len, self.n_hidden)
 
+        hard_tanh = torch.nn.Hardtanh(min_val=0.0, max_val=1.0)
+
         # NB: doesn't work on CUDA; due to FORCE training, h_0 is updated
         # regularly in time and therefore lives on the CPU
         for batch_idx in range(batch_size):
             # each batch can theoretically have distinct initial conditions,
             # injected noise, or inputs
-            # r_t_minus_1 = torch.ones(self.n_hidden) * (0.5 / (1 + self.beta * 0.5 * self.tau_depr))
             if r_0 is None:
                 r_t_minus_1 = torch.ones(self.n_hidden)
             else:
@@ -102,7 +103,6 @@ class RNN(nn.Module):
             # begin integration over time
             for t_idx in range(0, seq_len):
                 # init empty vector for current time step
-                # dhdt = torch.zeros(self.n_hidden)
                 self.noise.normal_(0, self.noise_std)
 
                 # pre-syn STP: depletion of resources (depression)
@@ -110,12 +110,12 @@ class RNN(nn.Module):
                     # silence the effect of syn depression
                     r_t = torch.ones(self.n_hidden)
                 else:
-                    # drdt = ((1 - r_t_minus_1) / self.tau_depr
-                    #         - self.beta * u_t_minus_1 * r_t_minus_1 *
-                    #         (1 + h_transfer) / 2)
-                    drdt = ((self.p_rel - r_t_minus_1) / self.tau_depr
-                            - self.beta * r_t_minus_1 * (1 + h_transfer) / 2)
-                    r_t = r_t_minus_1 + drdt * dt
+                    drdt = ((1 - r_t_minus_1) / self.tau_depr
+                            - self.beta * u_t_minus_1 * r_t_minus_1 * h_transfer)
+                    # drdt = ((self.p_rel - r_t_minus_1) / self.tau_depr
+                    #         - self.beta * r_t_minus_1 * h_transfer)
+                            # + self.beta * self.p_rel / 2)
+                    r_t = hard_tanh(r_t_minus_1 + drdt * dt)  # impose bounds
                 r_t_all[batch_idx, t_idx, :] = r_t.clone()
 
                 # pre-syn STP: augmentation of utilization (facilitation)
@@ -124,9 +124,8 @@ class RNN(nn.Module):
                     u_t = torch.ones(self.n_hidden)
                 else:
                     dudt = ((self.p_rel - u_t_minus_1) / self.tau_facil
-                            + self.beta * (1 - u_t_minus_1) *
-                            (1 + h_transfer) / 2)
-                    u_t = u_t_minus_1 + dudt * dt
+                            + self.beta * self.p_rel * (1 - u_t_minus_1) * h_transfer)
+                    u_t = hard_tanh(u_t_minus_1 + dudt * dt)  # impose bounds
                 u_t_all[batch_idx, t_idx, :] = u_t.clone()
 
                 # calculate total transfer weight
@@ -135,10 +134,10 @@ class RNN(nn.Module):
                                     self.gain * self.W_hh * self.W_hh_mask)
 
                 # post-synaptic integration
-                dhdt = (-h_t_minus_1 +
-                        x[batch_idx, t_idx, :] @ self.W_ih.T +
-                        h_transfer @ effective_weight.T +
-                        self.noise) / self.tau
+                dhdt = (-h_t_minus_1
+                        + x[batch_idx, t_idx, :] @ self.W_ih.T
+                        + h_transfer @ effective_weight.T
+                        + self.noise) / self.tau
                 dhdhtminus1_all[batch_idx, t_idx, :] = dhdt / dhdt_minus_1
                 h_t = h_t_minus_1 + dhdt * dt
                 h_t_all[batch_idx, t_idx, :] = h_t.clone()
