0b914523322f7f8f40999b8a1f5b2b19f082f48d
Ryan Thorpe <ryvthorpe@gmail.com>
2025-05-08 15:30:06 -0600

    return tensor value of loss in test_and_get_stats()

--- train.py
+++ train.py
@@ -239,6 +239,6 @@ def test_and_get_stats(inputs, targets, times, model, loss_fn, h_0, r_0, u_0,
 
     # calculate metrics-of-interest
     n_dim = est_dimensionality(hidden_batch)
-    stats = dict(loss=loss, dimensionality=n_dim)
+    stats = dict(loss=loss.item(), dimensionality=n_dim)
 
     return torch.tanh(h_t).cpu(), z_t.cpu(), stats
