a195a46c6eaf5d0318925d3392e8ae0640e522e9
Ryan Thorpe <ryvthorpe@gmail.com>
2025-05-19 12:01:12 -0600

    remove old plot_state_traj() function

--- viz.py
+++ viz.py
@@ -64,39 +64,6 @@ def plot_inputs_outputs(inputs, outputs, times, rec_traj=None, targets=None):
     return fig
 
 
-def plot_state_traj(h_units, outputs, targets, times):
-    # NB: assumes a single batch/trial
-    n_times, n_hidden = h_units.shape
-    n_outputs = outputs.shape[1]
-
-    time_mask = times > 0
-    times_after_zero = times[time_mask]
-
-    n_hidden_plot = 10  # number of hidden units to plot
-    if n_hidden < 10:
-        n_hidden_plot = n_hidden
-
-    # colors = plt.cm.binary(np.linspace(0.2, 1, n_outputs))
-    colors = plt.cm.viridis_r(np.linspace(0, 1, n_outputs))
-    fig, axes = plt.subplots(2, 1, sharex=True, figsize=(6, 6))
-
-    # recurrent unit trajectories
-    axes[0].plot(times, h_units[:, :n_hidden_plot], c='k')
-    axes[0].set_ylabel('normalized\nfiring rate (a.u.)')
-
-    # outputs
-    for out_idx, color in zip(range(n_outputs), colors):
-        axes[1].plot(times, outputs[:, out_idx], c=color, lw=2)
-        axes[1].plot(times_after_zero, targets[time_mask, out_idx], c=color,
-                     lw=2, ls=':')
-
-    axes[1].set_xticks(np.arange(0, 1.2, 0.2))
-    axes[1].set_xlabel('time (s)')
-    axes[1].set_ylabel('normalized\nfiring rate (a.u.)')
-
-    return fig
-
-
 def plot_divergence(divergence, delay_times, perturb_mags, ax):
     colors = plt.cm.inferno_r(np.linspace(0.1, 1, len(perturb_mags)))
     for perturb_mag_idx, perturb_divergence in enumerate(divergence):
