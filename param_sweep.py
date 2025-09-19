8e0892c707f229ffe35d8018e7aab06b6f8bcd79
Ryan Thorpe <ryvthorpe@gmail.com>
2025-09-19 14:56:36 -0600

    refactor param_sweep.py to merely simulated, not train

--- param_sweep.py
+++ param_sweep.py
@@ -13,12 +13,10 @@ from torch import nn
 
 from utils import get_gaussian_targets, get_commit_hash, get_timestamp
 from models import RNN
-from train import test_and_get_stats, train_bptt
+from train import sim_batch
 
 # tau, include_stp, noise_tau, noise_std, include_corr_noise, p_rel_range
 sim_params_all = [[0.01, False, 0.01, 0.0, False, 2],
-                  [0.05, False, 0.01, 0.0, False, 2],
-                  [0.01, False, 0.01, 0.0, False, 2],
                   [0.01, True, 0.01, 0.0, False, 2],
                   [0.01, False, 0.01, 1e-6, False, 2],
                   [0.01, True, 0.01, 1e-6, False, 2],
@@ -28,8 +26,7 @@ sim_params_all = [[0.01, False, 0.01, 0.0, False, 2],
                   [0.01, True, 0.01, 1e-6, False, 0]]
 n_random_nets = 30
 n_jobs = 30
-n_trials = 1000
-return_trials = (0, 100, n_trials)
+n_trials = 100
 output_dir = '/projects/ryth7446/time_coding_output'
 # output_dir = '/home/ryan/Desktop'
 
@@ -41,12 +38,12 @@ def adjust_gain_stp(model, times, dt, n_steps=8):
     '''
 
     # define inputs (for contextual modulation / recurrent perturbations)
-    n_batches = 1
+    # NB: simulates a single trial between adjustments
     n_inputs = model.n_inputs
     n_hidden = model.n_hidden
     n_outputs = model.n_outputs
     n_times = len(times)
-    inputs = torch.zeros((n_batches, n_times, n_inputs))
+    inputs = torch.zeros((1, n_times, n_inputs))
     perturb_dur = 0.05  # 50 ms
     perturb_win_mask = np.logical_and(times > -perturb_dur, times < 0)
     inputs[:, perturb_win_mask, :] = 0.1
@@ -59,16 +56,16 @@ def adjust_gain_stp(model, times, dt, n_steps=8):
     targ_std = 0.05 / np.sqrt(2)  # ~35 ms
     # tile center of target delays spanning sim duration (minus margins)
     delay_times = np.linspace(0.1, 1.0, n_outputs)
-    targets = get_gaussian_targets(n_batches, delay_times, times, targ_std)
+    targets = get_gaussian_targets(1, delay_times, times, targ_std)
 
     # set initial conditions of recurrent units fixed across iterations of
     # training and testing
     h_0 = torch.zeros(n_hidden)  # steady-state for postsyn activity var
-    h_0 = torch.tile(h_0, (n_batches, 1))  # replicate for each batch
+    h_0 = torch.tile(h_0, (1, 1))  # replicate for each batch
     r_0 = torch.ones(n_hidden)  # steady-state for depression var
-    r_0 = torch.tile(r_0, (n_batches, 1))
+    r_0 = torch.tile(r_0, (1, 1))
     u_0 = model.p_rel.detach()  # steady-state for facilitation var
-    u_0 = torch.tile(u_0, (n_batches, 1))
+    u_0 = torch.tile(u_0, (1, 1))
 
     # run opt routine
     # move to desired device
@@ -87,19 +84,17 @@ def adjust_gain_stp(model, times, dt, n_steps=8):
     # slowly inclrease STP beta param, turning up gain adjustment accordingly
     beta_steps = torch.linspace(0, model.beta, n_steps + 1)
     gain_vals = list()
-    model.eval()
     for beta in beta_steps:
         model.beta = beta
 
         # adjust beta twice for each incremental increase in beta
         for update_idx in range(2):
-            state_vars_, _ = test_and_get_stats(inputs, targets, times,
-                                                model, loss_fn, h_0, r_0, u_0,
-                                                dt=dt, include_stp=True,
-                                                noise_tau=noise_tau,
-                                                noise_std=noise_std,
-                                                include_corr_noise=include_corr_noise,
-                                                plot=False)
+            state_vars_, _ = sim_batch(inputs, targets, times, model, loss_fn,
+                                       h_0, r_0, u_0, dt=dt,
+                                       include_stp=True,
+                                       noise_tau=noise_tau,
+                                       noise_std=noise_std,
+                                       include_corr_noise=include_corr_noise)
             ext_in_, hidden_sr_t_, r_t_, u_t_, output_sr_t_ = state_vars_
             syn_eff_ = r_t_ * u_t_
             adjustment_fctr = model.p_rel.mean() / syn_eff_.mean()
@@ -109,24 +104,22 @@ def adjust_gain_stp(model, times, dt, n_steps=8):
     return gain_vals
 
 
-def train_net(model, optimizer, loss_fn, times,
-              tau, include_stp, noise_tau, noise_std, include_corr_noise,
-              p_rel_range, adjusted_gain,
-              n_trials=1000, return_trials=(0, 100, 1000), dt=1e-3,
-              device='cpu'):
+def sim_net(model, loss_fn, times,
+            tau, include_stp, noise_tau, noise_std, include_corr_noise,
+            p_rel_range, adjusted_gain,
+            n_trials=100, dt=1e-3,
+            device='cpu'):
     '''Train current instantiation of network model.
 
-    Returns network parameters and variables after return_trials number of
-    training trials.
+    Returns network parameters and variables for a batch of sim trials.
     '''
 
     # define inputs
-    n_batches = 1
     n_inputs = model.n_inputs
     n_hidden = model.n_hidden
     n_outputs = model.n_outputs
     n_times = len(times)
-    inputs = torch.zeros((n_batches, n_times, n_inputs))
+    inputs = torch.zeros((n_trials, n_times, n_inputs))
     perturb_dur = 0.05  # 50 ms
     perturb_win_mask = np.logical_and(times > -perturb_dur, times < 0)
     inputs[:, perturb_win_mask, :] = 0.1
@@ -152,19 +145,19 @@ def train_net(model, optimizer, loss_fn, times,
     targ_std = 0.05 / np.sqrt(2)  # ~35 ms
     # tile center of target delays spanning sim duration (minus margins)
     delay_times = np.linspace(0.1, 1.0, n_outputs)
-    targets = get_gaussian_targets(n_batches, delay_times, times, targ_std)
+    targets = get_gaussian_targets(n_trials, delay_times, times, targ_std)
 
     # set initial conditions of recurrent units fixed across iterations of
     # training and testing
     # h_0 = torch.tensor(sol.x[:n_hidden], dtype=torch.float32)
     h_0 = torch.zeros(n_hidden)
-    h_0 = torch.tile(h_0, (n_batches, 1))  # replicate for each batch
+    h_0 = torch.tile(h_0, (n_trials, 1))  # replicate for each batch
     # r_0 = torch.tensor(sol.x[n_hidden:2 * n_hidden], dtype=torch.float32)
     r_0 = torch.ones(n_hidden) 
-    r_0 = torch.tile(r_0, (n_batches, 1))
+    r_0 = torch.tile(r_0, (n_trials, 1))
     # u_0 = torch.tensor(sol.x[2 * n_hidden:3 * n_hidden], dtype=torch.float32)
     u_0 = model.p_rel.detach()
-    u_0 = torch.tile(u_0, (n_batches, 1))
+    u_0 = torch.tile(u_0, (n_trials, 1))
 
     # run opt routine
     # move to desired device
@@ -174,65 +167,20 @@ def train_net(model, optimizer, loss_fn, times,
     h_0 = h_0.to(device)
     r_0 = r_0.to(device)
     u_0 = u_0.to(device)
+ 
+    state_vars_raw, _ = sim_batch(
+        inputs, targets, times, model, loss_fn, h_0, r_0, u_0, dt,
+        include_stp=include_stp, noise_tau=noise_tau,
+        noise_std=noise_std, include_corr_noise=include_corr_noise)
+    
+    ext_in, h_t, r_t, u_t, z_t = state_vars_raw
+    state_vars = (ext_in.detach().numpy(),
+                  model.transfer_func(h_t).detach().numpy(),
+                  r_t.detach().numpy(),
+                  u_t.detach().numpy(),
+                  z_t.detach().numpy())
 
-    optimizer.zero_grad()
-
-    # train model weights
-    losses = list()
-    params = list()
-    state_vars = list()
-    for trial_idx in range(n_trials):
-
-        loss, init_params, state_vars_raw = train_bptt(
-            inputs, targets, times, model, loss_fn,
-            optimizer, h_0, r_0, u_0, dt=dt,
-            include_stp=include_stp, noise_tau=noise_tau,
-            noise_std=noise_std, include_corr_noise=include_corr_noise)
-
-        if trial_idx in return_trials:
-            params.append(init_params)
-
-            ext_in, h_t, r_t, u_t, z_t = state_vars_raw
-            # select 1st (and only) batch trial
-            state_vars_processed = (ext_in.detach().numpy()[0],
-                                    model.transfer_func(h_t).detach().numpy()[0],
-                                    r_t.detach().numpy()[0],
-                                    u_t.detach().numpy()[0],
-                                    z_t.detach().numpy()[0])
-            state_vars.append(state_vars_processed)
-
-        losses.append(loss)
-
-    # investigate fitted model after training; test output using iid noise
-    # averaged across 10 batch trials
-    inputs_test = torch.tile(inputs, dims=(10, 1, 1))
-    targets_test = torch.tile(targets, dims=(10, 1, 1))
-    h_0_test = torch.tile(h_0, dims=(10, 1))
-    r_0_test = torch.tile(r_0, dims=(10, 1))
-    u_0_test = torch.tile(u_0, dims=(10, 1))
-    state_vars_post_raw, sim_stats_post = test_and_get_stats(
-        inputs_test, targets_test, times, model, loss_fn,
-        h_0_test, r_0_test, u_0_test, dt=dt,
-        include_stp=include_stp, noise_tau=noise_tau, noise_std=noise_std,
-        include_corr_noise=include_corr_noise, plot=False)
-
-    losses.append(sim_stats_post['loss'])
-
-    if trial_idx + 1 in return_trials:
-        final_params = [param.detach().numpy() for param in model.parameters()
-                        if param.requires_grad]
-        params.append(final_params)
-
-        ext_in, h_t, r_t, u_t, z_t = state_vars_post_raw
-        # select 1st batch trial
-        state_vars_post_processed = (ext_in.detach().numpy()[0],
-                                     model.transfer_func(h_t).detach().numpy()[0],
-                                     r_t.detach().numpy()[0],
-                                     u_t.detach().numpy()[0],
-                                     z_t.detach().numpy()[0])
-        state_vars.append(state_vars_post_processed)
-
-    return losses, params, state_vars
+    return state_vars
 
 
 def loss_fn(output, target):
@@ -249,7 +197,7 @@ def eval_net_instance(sim_params_all, net_idx):
     n_times = len(times)
 
     # create network and scale up gain to accomodate STP
-    n_inputs, n_hidden, n_outputs = 1, 1000, 10
+    n_inputs, n_hidden, n_outputs = 1, 500, 10
     sample_new_net = True
     while sample_new_net is True:
         # instantiate network
@@ -270,47 +218,27 @@ def eval_net_instance(sim_params_all, net_idx):
         else:
             print('warning: resampling network')
 
-    # save baseline and adjusted gain values for later
-    base_gain = gains[0]
+    # save adjusted gain value for later
     adjusted_gain = gains[-1]
 
-    # instantiate optimizer and link to RNN output weights for tuning
-    optimizer = torch.optim.SGD(model.parameters(), lr=1e-3)
-
-    # save inital state of tuned model parameters
-    init_W_hz = model.W_hz.data.detach().clone()
-    init_offset_hz = model.offset_hz.data.detach().clone()
-
     n_sims = len(sim_params_all)
-    n_return_trials = len(return_trials)
-    losses_all = np.empty((n_sims, n_trials + 1), dtype=np.float32)
-    model_params_all = {'W_hz': np.empty((n_sims, n_return_trials, n_outputs,
+    model_params = {'W_hz': model.W_hz.data.detach().clone(),
+                    'offset_hz': model.offset_hz.data.detach().clone()}
+    state_vars_all = {'ext_in': np.empty((n_sims, n_trials, n_times,
                                           n_hidden), dtype=np.float32),
-                        'offset_hz': np.empty((n_sims, n_return_trials,
-                                               n_outputs), dtype=np.float32)}
-    state_vars_all = {'ext_in': np.empty((n_sims, n_return_trials, n_times,
-                                          n_hidden), dtype=np.float32),
-                      'h_sp': np.empty((n_sims, n_return_trials, n_times,
+                      'h_sp': np.empty((n_sims, n_trials, n_times,
                                        n_hidden), dtype=np.float32),
-                      'r': np.empty((n_sims, n_return_trials, n_times,
+                      'r': np.empty((n_sims, n_trials, n_times,
                                        n_hidden), dtype=np.float32),
-                      'u': np.empty((n_sims, n_return_trials, n_times,
-                                       n_hidden), dtype=np.float32),
-                      'z': np.empty((n_sims, n_return_trials, n_times,
-                                       n_outputs), dtype=np.float32)}
+                      'u': np.empty((n_sims, n_trials, n_times,
+                                       n_hidden), dtype=np.float32)}
 
     for sim_idx, sim_params in enumerate(sim_params_all):
         tau, include_stp, noise_tau, noise_std, include_corr_noise, p_rel_range = sim_params
 
-        # reset output weights
-        with torch.no_grad():
-            model.W_hz.copy_(init_W_hz)
-            model.offset_hz.copy_(init_offset_hz)
-
-        print(f'start: training for network condition {sim_idx}')
-        losses, model_params, state_vars = train_net(
+        print(f'start: simulated trials for network condition {sim_idx}')
+        state_vars = sim_net(
             model=model,
-            optimizer=optimizer,
             loss_fn=loss_fn,
             times=times,
             tau=tau,
@@ -321,31 +249,22 @@ def eval_net_instance(sim_params_all, net_idx):
             p_rel_range=p_rel_range,
             adjusted_gain=adjusted_gain,
             n_trials=n_trials,
-            return_trials=return_trials,
             dt=dt,
             device=device)
-        print(f'end: training for network condition {sim_idx}')
+        print(f'end: simulated trials for network condition {sim_idx}')
 
-        losses_all[sim_idx, :] = losses
-        # convert to ragged array, then parse according to parameter name
-        # model_params = np.array(model_params, dtype=object)
-        model_params_all['W_hz'][sim_idx] = np.array([model_param[0] for model_param in model_params])
-        model_params_all['offset_hz'][sim_idx] = np.array([model_param[1] for model_param in model_params])
-        # convert to ragged array, then parse according to variable name
-        state_vars_all['ext_in'][sim_idx] = np.array([state_var[0] for state_var in state_vars])
-        state_vars_all['h_sp'][sim_idx] = np.array([state_var[1] for state_var in state_vars])
-        state_vars_all['r'][sim_idx] = np.array([state_var[2] for state_var in state_vars])
-        state_vars_all['u'][sim_idx] = np.array([state_var[3] for state_var in state_vars])
-        state_vars_all['z'][sim_idx] = np.array([state_var[4] for state_var in state_vars])
+        state_vars_all['ext_in'][sim_idx] = state_vars[0]
+        state_vars_all['h_sp'][sim_idx] = state_vars[1]
+        state_vars_all['r'][sim_idx] = state_vars[2]
+        state_vars_all['u'][sim_idx] = state_vars[3]
 
     # save simulation data in HDF5 file within output directory
     fname_local = ('sim_data_' + f'net{net_idx:02d}_' +
                    get_commit_hash() + '_' + get_timestamp() + '.hdf5')
     fname_absolute = op.join(output_dir, fname_local)
     with h5py.File(fname_absolute, 'w') as f_write:
-        f_write.create_dataset('losses', data=losses_all)
         f_write.create_dataset('sim_params', data=np.array(sim_params_all))
-        for key, val in model_params_all.items():
+        for key, val in model_params.items():
             f_write.create_dataset(key, data=val)
         for key, val in state_vars_all.items():
             f_write.create_dataset(key, data=val)
